b245f3d9ab641ed0c8ade60f118d419b91ae8fbb
Amadi Emmanuel <larryamadi223@gmail.com>
2025-02-23 09:49:22 -0500

    Update app.js

--- app.js
+++ app.js
@@ -17,5 +17,5 @@ app.use('/api/admin', adminRoutes);
 app.use('/api/teacher', teacherRoutes);
 app.use('/api/student', studentRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
