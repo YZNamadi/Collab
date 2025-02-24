d811e6a4289dce28997f5a3f995fb386aaeb1220
Ernestenejo <eenejo69@gmail.com>
2025-02-24 03:31:09 +0100

    modified the route and added another file to the authorization file to the middleware folder

---
+++ middleware/authorization.js
@@ -0,0 +1,71 @@
+const jwt = require('jsonwebtoken');
+const userModel = require('../models/admin')
+
+exports.checkRole = async (req, res, next) =>{
+    if(!req.headers.authorization){
+        return res.status(404).json({
+            message:"Kindly Login to perform this Action"
+        })
+    }
+    const checkToken = req.headers.authorization.split(" ")[1]
+
+    
+   const tokenOwner = await jwt.verify(checkToken, "secret_key", (error, data)=>{
+       if(error){ 
+        return res.status(400).json({
+            message: error.message
+        })
+
+        }
+        return data
+    })
+    console.log(tokenOwner )
+    const checkUser = await userModel.findById(tokenOwner.id)
+    if(!checkUser){
+        return res.status(404).json({
+            message: "School Not Found"
+        })
+    }else if(checkUser.isSuperAdmin === true){
+        next()
+       
+    }else{
+        return res.status(401).json({
+            message: "You Are Not Authorize for this Action"
+        })
+    }
+}
+exports.adminRole = async (req, res, next) =>{
+    if(!req.headers.authorization){
+        return res.status(404).json({
+            message:"Kindly Login to perform this Action"
+        })
+    }
+    const checkToken = req.headers.authorization.split(" ")[1]
+
+    
+   const tokenOwner = await jwt.verify(checkToken, "secret_key", (error, data)=>{
+       if(error){ 
+        return res.status(400).json({
+            message: error.message
+        })
+
+        }
+        return data
+    })
+    console.log(tokenOwner )
+    const checkUser = await userModel.findById(tokenOwner.id)
+    if(!checkUser){
+        return res.status(404).json({
+            message: "School Not Found"
+        })
+    }else if(checkUser.isAdmin === true){
+        next()
+       
+    }else{
+        return res.status(401).json({
+            message: "You Are Not Authorize for this Action"
+        })
+    }
+}
+
+
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,12 +1,13 @@
 const { createUser, verifyMail,getallUser,getOneUser,changePassword,forgotPassword,resetPassword } = require('../controllers/adminController')
 const {checkLogins} = require('../middleware/authMiddleware')
+const {checkRole} = require('../middleware/authorization')
 
 const router = require ('express').Router()
 
-router.post('/useer',checkLogins, createUser)
+router.post('/useer',checkRole, checkLogins, createUser)
 router.get('/mail/:id', verifyMail)
-router.get('/user',getallUser)
-router.get('/one/:id',getOneUser)
+router.get('/user',checkRole,getallUser)
+router.get('/one/:id',checkRole, getOneUser)
 router.post("/change-password", changePassword);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password/:token", resetPassword);
