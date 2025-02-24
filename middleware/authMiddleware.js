9992bcec8ea451b17cff11b519cdc5233e4ac63c
Amadi Emmanuel <larryamadi223@gmail.com>
2025-02-24 03:51:21 -0500

    Amadi

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,30 +1,18 @@
 const jwt = require('jsonwebtoken');
-const userModel = require('../models/admin')
+require('dotenv').config();
 
-exports.checkLogins = async (req, res, next) =>{
-    if(!req.headers.authorization){
-        return res.status(404).json({
-            message:"Kindly Login to perform this Action"
-        })
-    }
-    const checkToken = req.headers.authorization.split(" ")[1]
+//to verify JWT token
+function authenticateToken(req, res, next) {
+    const token = req.header('Authorization')?.split(' ')[1]; 
+    if (!token) return res.status(401).json({ message: 'Access denied. No token provided.' });
 
-    
-   const tokenOwner = await jwt.verify(checkToken, "secret_key", (error, data)=>{
-       if(error){ 
-        return res.status(400).json({
-            message: error.message
-        })
-        
-        }
-        return data
-    })
-    console.log(tokenOwner )
-    const checkUser = await userModel.findById(tokenOwner.id)
-    if(!checkUser){
-        return res.status(404).json({
-            message: "School Not Found"
-        })
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET); 
+        req.user = decoded; 
+        next(); 
+    } catch (err) {
+        res.status(403).json({ message: 'Invalid token.' });
     }
-    next()
 }
+
+module.exports = authenticateToken;
